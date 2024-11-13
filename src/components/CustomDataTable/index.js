e6ee18c3b4928b30f5dbde468197d2b481505296
AsrithaAllam <asrithaniranjana@trinovatek.com>
2024-11-13 17:56:05 +0530

    serach functionality

--- src/components/CustomDataTable/index.js
+++ src/components/CustomDataTable/index.js
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
-import Loader from "../Loader";
 
 const CustomDataTable = ({ columns, data }) => {
-  const modifiedColumns = [
-    ...columns,
-  ];
+  const [search, setSearch] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  // const filteredData = data.filter(row =>
+
+  // Object.values(row).some(value =>
+  //   value && value.toString().toLowerCase().includes(search.toLowerCase())
+  // )
+  // );
 
   const customStyles = {
     rows: {
       style: {
-        // fontFamily: 'Poppins',
-        fontSize:'0.8rem',
+        fontSize: "0.8rem",
       },
     },
     headCells: {
       style: {
         fontSize: "0.8rem",
-        // height: "7vh",
-        backgroundColor:"black",
-        color: "white"
+        backgroundColor: "black",
+        color: "white",
       },
     },
     cells: {
@@ -30,30 +33,47 @@ const CustomDataTable = ({ columns, data }) => {
     pagination: {
       style: {
         fontSize: "0.8rem",
-        // height: "7vh"
       },
     },
   };
+ 
+  useEffect(() => {
+    if (search !== "") {
+      const dummyData = data.filter((row) => {
+        
+        if (row.firstName.includes(search)) {
+          return row;
+        }
+      });
+      setFilteredData(dummyData);
+    } else {
+      setFilteredData(data);
+      console.log(data, "hi");
+    }
+  }, [search]);
 
   return (
     <div>
-      <input className="flex float-end h-6 p-4 m-2 border-2 border-black rounded" placeholder="search" type="text" />
-    
-    <DataTable
-      className="border-gray-400 border-2 radius-xl customBoder"
-      columns={modifiedColumns}
-      data={data}
-      pagination
-      striped
-      highlightOnHover
-      customStyles={customStyles}
-    />
+      <input
+        className="flex float-end h-6 p-4 m-2 border-2 border-black rounded"
+        placeholder="Search"
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <DataTable
+        className="border-gray-400 border-2 radius-xl customBoder"
+        columns={columns}
+        data={filteredData}
+        pagination
+        striped
+        highlightOnHover
+        customStyles={customStyles}
+        // noDataComponent={filteredData.length === 0 ? "No matching data found" : ""}
+      />
     </div>
   );
 };
 
 export default CustomDataTable;
-
-
-
-
